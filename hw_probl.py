79fef59f33ee743e18e4f9565aecf00bc53fbf45
Sanchita Saxena <sanc.sax@gmail.com>
2016-08-12 00:10:40 +0000

    did the second ds but still can't figure out what Q4 was asking. Going to try to see what Q4 is asking for.

--- hw_probl.py
+++ hw_probl.py
@@ -1,5 +1,5 @@
 ##### GIVEN #####
-# fruit name    #         
+# fruit name    #      
 # city name     #
 # price         #
 ##### GIVEN #####
@@ -7,34 +7,23 @@
 
 #Q1: What DS would you use?
 
-# First try, I'm using a dictionary of dictionaries because the keys will be great ways
-# to look up values for the fruit name, city, and price.
+# First try, I'm using a list of dictionaries: keys will give me an easier time
+# going through the data and it'll be easier to get the values I want!
 # Second try, I'll be using a list of tuples because I can iterate by using indices
-#
+# or I can unpack the tuples.
 
 #Q2: For that DS, find the...
 #    1) cheapest fruit (name)
 #    2) cheapest fruit (city)
 #    3) which city has the most fruits (type of)
-#    4) wich is the most abundant fruit in the US
-#Q2: Now use the other DS too!
-
+# >> 4) wich is the most abundant fruit in the US << couldn't figure it out
 
 fruit_dicts = [{'name': 'orange', 'city': 'SF', 'price': 5},
                 {'name': 'orange', 'city': 'LA', 'price': 6},
                 {'name': 'apple', 'city': 'NY', 'price': 2},
                 {'name': 'apple', 'city': 'SF', 'price': 3}]
 
-fruit_tuple = [('orange', 'SF', 5),
-               ('orange', 'LA', 6),
-               ('apple', 'NY', 2),
-               ('apple', 'SF', 3)]
 
-
-#create a list that will hold the price key values and their
-#corrusponding fruit names as a tuple
-
-# Using the first DS
 def cheapest_fruit_name(fruit_dicts):
 
     cheapest_fruit = fruit_dicts[0]
@@ -79,8 +68,6 @@ def cheapest_fruit_city(fruit_dicts):
 
 def city_with_most_fruits_type(fruit_dicts):
 
-    # the city with the most fruit, and what kinds of fruits
-    # output I want {'SF': ['apple','orange']}
     cities = {}
     types_of_fruits = []
 
@@ -104,14 +91,86 @@ def city_with_most_fruits_type(fruit_dicts):
 
     return (max_fruits_city, max_fruits)
 
+# def most_abundant_fruit(): # WHAT ARE YOU ASKING?!
 
+
+print cheapest_fruit_name(fruit_dicts)
+print cheapest_fruit_city(fruit_dicts)
 print city_with_most_fruits_type(fruit_dicts)
 
+#Q2: Now use the other DS too!
+
+fruit_tuples = [('orange', 'SF', 5),
+               ('orange', 'LA', 6),
+               ('apple', 'NY', 2),
+               ('apple', 'SF', 3)]
+
+
+def cheapest_fruit_tuple(fruit_tuples):
+
+    lowest_price = fruit_tuples[0]
+    for item in fruit_tuples:
+        fruit, city, price = item
+        if price < lowest_price:
+            lowest_price = price
+            cheapest_fruit = fruit
+
+    return cheapest_fruit
+
+
+def cheapest_city_tuple(fruit_tuples):
+
+    lowest_price = fruit_tuples[0]
+    for item in fruit_tuples:
+        fruit, city, price = item
+        if price < lowest_price:
+            lowest_price = price
+            city_where_cheapest = city
+    return city_where_cheapest
+
+print cheapest_city_tuple(fruit_tuples)
+
+def city_with_most_fruits_tuple(fruit_tuples):
+
+    cities = {}
+    types_of_fruits = []
+
+    for item in fruit_tuples:
+        fruit, city, price = item
+        key = city
+        value = fruit
+        types_of_fruits.append(value)
+
+        if key not in cities:
+            cities[key] = []
+
+        cities[key].append(value)
+
+    max_fruits_length = 0
+
+    for city, value in cities.items():
+        if len(value) > max_fruits_length:
+            max_fruits_length = len(value)
+            max_fruits_city = city
+            max_fruits = value
+
+    return (max_fruits_city, max_fruits)
+
+# def most_abundant_fruit_tuple(): # WHAT ARE YOU ASKING?!
+
+print cheapest_fruit_tuple(fruit_tuples)
+print cheapest_city_tuple(fruit_tuples)
+print city_with_most_fruits_tuple(fruit_tuples)
 
 
 
 
-    # def most_abundant_fruit():
-    # have no idea how to even start this one
 
-############################## Using the second DS ##############################
+
+
+
+
+
+
+
+
